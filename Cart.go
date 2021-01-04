02bae69ffe17d1f07cd52483d82f4b4b66ca77c0
Julian Chu <yulang.chu@gmail.com>
2021-01-04 15:50:44 +0100

    add CalcShippingFeeFunc

--- Cart.go
+++ Cart.go
@@ -8,39 +8,42 @@ var (
 	ErrShipperNotExist = errors.New("shipper not exist")
 )
 
-type Product struct {
-	Length float64
-	Width  float64
-	Height float64
-	Weight float64
-}
-
-func NewProduct(length float64, width float64, height float64, weight float64) *Product {
-	return &Product{Length: length, Width: width, Height: height, Weight: weight}
-}
+type CalcShippingFeeFunc func(*Product) (float64, error)
 
 func (c Cart) ShippingFee(shipper string, p *Product) (float64, error) {
 	switch shipper {
 	case "UPS":
-		if p.Weight > 20 {
-			return 500, nil
-		}
-		return 100 + p.Weight*10, nil
+		return UPS(p)
 	case "FedEx":
-		size := p.Length * p.Width * p.Height
-		if p.Length > 100 || p.Width > 100 || p.Height > 100 {
-			return size*0.00002*1100 + 500, nil
-		}
-		return size * 0.00002 * 1200, nil
+		return FedEx(p)
 	case "Post office":
-		feeByWeight := 80 + p.Weight*10
-		size := p.Length * p.Width * p.Height
-		feeBySize := size * 0.00002 * 1100
-		if feeByWeight < feeBySize {
-			return feeByWeight, nil
-		}
-		return feeBySize, nil
+		return Post(p)
 	default:
 		return -1, ErrShipperNotExist
 	}
+}
+
+func Post(p *Product) (float64, error) {
+	feeByWeight := 80 + p.Weight*10
+	size := p.Length * p.Width * p.Height
+	feeBySize := size * 0.00002 * 1100
+	if feeByWeight < feeBySize {
+		return feeByWeight, nil
+	}
+	return feeBySize, nil
+}
+
+func FedEx(p *Product) (float64, error) {
+	size := p.Length * p.Width * p.Height
+	if p.Length > 100 || p.Width > 100 || p.Height > 100 {
+		return size*0.00002*1100 + 500, nil
+	}
+	return size * 0.00002 * 1200, nil
+}
+
+func UPS(p *Product) (float64, error) {
+	if p.Weight > 20 {
+		return 500, nil
+	}
+	return 100 + p.Weight*10, nil
 }
---
+++ Product.go
@@ -0,0 +1,12 @@
+package strategy_sample_go
+
+type Product struct {
+	Length float64
+	Width  float64
+	Height float64
+	Weight float64
+}
+
+func NewProduct(length float64, width float64, height float64, weight float64) *Product {
+	return &Product{Length: length, Width: width, Height: height, Weight: weight}
+}
